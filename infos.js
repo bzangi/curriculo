ff97e4856b43d62b2e8c1dd0fd75ed7e03c4f8b7
bzangi <bruno_zangi@hotmail.com>
2021-10-21 23:28:17 -0300

    aula 6

---
+++ infos.js
@@ -0,0 +1,8 @@
+var nome = "Bruno Zangirolami "
+var arroba = "@brunoDev "
+var polo = "Santana "
+document.write(" ", nome);
+document.write("Twitter: ", arroba)
+document.write("Instagram: ", arroba)
+document.write("LinkedIn: ", arroba)
+document.write("Localização - Polo: ", polo)
